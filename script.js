d477b443b56d270ebc45d467d55abf91fff33af4
rameshk978433@gmail.com <rameshk978433@gmail.com>
2023-08-17 00:19:25 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+function scroll() {
+    var header = document.getElementById('header') 
+    var scrollValue = window.scrollY;
+
+    if(scrollValue < 100) {
+        header.classList.remove('scroll')
+    } else {
+        header.classList.add('scroll')
+    }
+}
+
+window.addEventListener('scroll', scroll)
+
+
 var typed = new Typed(".multiple-text", {
     strings: ["Mechanical Engineer", "Tools Designer", "3-D Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
 })
+
